65e8543ee2220a98914f8e704a751ff31cfa636d
MichelPezzat <57415636+MichelPezzat@users.noreply.github.com>
2021-05-06 19:38:03 -0500

    checkpoint res

--- neural_operations.py
+++ neural_operations.py
@@ -14,19 +14,20 @@ from thirdparty.inplaced_sync_batchnorm import SyncBatchNormSwish
 
 from utils import average_tensor
 from collections import OrderedDict
+from checkpoint import checkpoint
 
 BN_EPS = 1e-5
 SYNC_BN = True
 
 OPS = OrderedDict([
-    ('res_elu', lambda Cin, Cout, stride: ELUConv(Cin, Cout, 3, stride, 1)),
-    ('res_bnelu', lambda Cin, Cout, stride: BNELUConv(Cin, Cout, 3, stride, 1)),
-    ('res_bnswish', lambda Cin, Cout, stride: BNSwishConv(Cin, Cout, 3, stride, 1)),
-    ('res_bnswish5', lambda Cin, Cout, stride: BNSwishConv(Cin, Cout, 3, stride, 2, 2)),
-    ('mconv_e6k5g0', lambda Cin, Cout, stride: InvertedResidual(Cin, Cout, stride, ex=6, dil=1, k=5, g=0)),
-    ('mconv_e3k5g0', lambda Cin, Cout, stride: InvertedResidual(Cin, Cout, stride, ex=3, dil=1, k=5, g=0)),
-    ('mconv_e3k5g8', lambda Cin, Cout, stride: InvertedResidual(Cin, Cout, stride, ex=3, dil=1, k=5, g=8)),
-    ('mconv_e6k11g0', lambda Cin, Cout, stride: InvertedResidual(Cin, Cout, stride, ex=6, dil=1, k=11, g=0)),
+    ('res_elu', lambda Cin, Cout, stride,checkpoint_res: ELUConv(Cin, Cout, 3, stride, 1,checkpoint_res)),
+    ('res_bnelu', lambda Cin, Cout, stride,checkpoint_res: BNELUConv(Cin, Cout, 3, stride, 1,checkpoint_res)),
+    ('res_bnswish', lambda Cin, Cout, stride,checkpoint_res: BNSwishConv(Cin, Cout, 3, stride, 1,checkpoint_res)),
+    ('res_bnswish5', lambda Cin, Cout, stride,checkpoint_res: BNSwishConv(Cin, Cout, 3, stride, 2, 2.checkpoint_res)),
+    ('mconv_e6k5g0', lambda Cin, Cout, stride,checkpoint_res: InvertedResidual(Cin, Cout, stride, ex=6, dil=1, k=5, g=0,checkpoint_res)),
+    ('mconv_e3k5g0', lambda Cin, Cout, stride,checkpoint_res: InvertedResidual(Cin, Cout, stride, ex=3, dil=1, k=5, g=0,checkpoint_res)),
+    ('mconv_e3k5g8', lambda Cin, Cout, stride,checkpoint_res: InvertedResidual(Cin, Cout, stride, ex=3, dil=1, k=5, g=8,checkpoint_res)),
+    ('mconv_e6k11g0', lambda Cin, Cout, stride,checkpoint_res: InvertedResidual(Cin, Cout, stride, ex=6, dil=1, k=11, g=0,checkpoint_res)),
 ])
 
 
