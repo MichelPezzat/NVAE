bc99cea7585e9fa2e6e256fc1e22876d940d9683
MichelPezzat <57415636+MichelPezzat@users.noreply.github.com>
2021-05-14 10:34:55 -0500

    debugging sintax

--- neural_operations.py
+++ neural_operations.py
@@ -21,7 +21,7 @@ BN_EPS = 1e-5
 SYNC_BN = True
 
 OPS = OrderedDict([
-    ('res_elu', lambda Cin, Cout, stride ELUConv(Cin, Cout, 3, stride, 1)),
+    ('res_elu', lambda Cin, Cout, stride: ELUConv(Cin, Cout, 3, stride, 1)),
     ('res_bnelu', lambda Cin, Cout, stride: BNELUConv(Cin, Cout, 3, stride, 1)),
     ('res_bnswish', lambda Cin, Cout, stride: BNSwishConv(Cin, Cout, 3, stride, 1)),
     ('res_bnswish5', lambda Cin, Cout, stride: BNSwishConv(Cin, Cout, 3, stride, 2, 2)),
