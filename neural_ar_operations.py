97cf37f67cbfced78f1c5eb40e7a2e504245ff95
MichelPezzat <57415636+MichelPezzat@users.noreply.github.com>
2021-05-17 20:25:17 -0500

    Update neural_ar_operations.py

--- neural_ar_operations.py
+++ neural_ar_operations.py
@@ -73,6 +73,7 @@ def norm(t, dim):
 
 def _convert_arconv_weights_to_fp16(l):
     if isinstance(l, ARConv1d):
+        print(l.weight.type())
         l.weight.data = l.weight.data.half()
         
         
@@ -94,6 +95,7 @@ class ARConv1d(nn.Conv1d):
             self.padding = dilation * (kernel_size - 1) // 2
         else:
             self.padding = 0
+        print(self.weight.type())
 
         # init weight normalizaition parameters
         init = torch.log(norm(self.weight, dim=[1, 2]).view(-1, 1, 1) + 1e-2)
